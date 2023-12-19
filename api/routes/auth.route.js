a61df9f180983f9db79d58169d6ed25802b2035f
Gloire Beya <gloweb78@gmail.com>
2023-12-19 08:57:48 +0200

    Create sign in API route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,12 +1,13 @@
 import express from 'express'
-import { signup } from '../controllers/auth.controller.js'
+import { signup, signin } from '../controllers/auth.controller.js'
 
 const route = express.Router()
 
 route.post('/signup', signup)
+route.post('/signin', signin)
 
 export default route
 
- 
+
 
 
