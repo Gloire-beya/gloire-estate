dd2a744d21c50416f338d8ebf7e3cfe756e0419a
Gloire Beya <gloweb78@gmail.com>
2023-12-20 12:47:14 +0200

    Complete user update functionality

--- client/src/redux/user/userSlice .js
+++ client/src/redux/user/userSlice .js
@@ -23,10 +23,32 @@ const userSlice = createSlice({
         signFailure: (state, action) => {
             state.error = action.payload
             state.loading = false
+        },
+
+        updateUserStart: (state) => {
+            state.loading = true
+        },
+
+        updateUserSuccess: (state, action) => {
+            state.currentUser = action.payload
+            state.loading = false
+            state.error = null
+        },
+
+        updateUserFailure: (state, action) => {
+            state.error = action.payload
+            state.loading = false
         }
     }
 })
 
-export const { signInStart, signSuccess, signFailure } = userSlice.actions
+export const {
+    signInStart,
+    signSuccess,
+    signFailure,
+    updateUserFailure,
+    updateUserStart,
+    updateUserSuccess
+} = userSlice.actions
 
 export default userSlice.reducer
