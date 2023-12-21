4f584cfb98850b184dc6f23ec525a809f622f9af
Gloire Beya <gloweb78@gmail.com>
2023-12-21 11:30:44 +0200

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -126,7 +126,7 @@ export default function Profile() {
 
   const handleListingDelete = async (listingId) => {
     try {
-      const res = await fetch(`/api/lisitng/delete/${listingId}`, {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
         method: 'DELETE'
       })
       const data = await res.json()
